f4d66aeb41b4c9350d776e4a1862231862b8855a
Jesús Viña <jesusvina13@gmail.com>
2024-06-10 14:35:02 -0400

    Update 01_hello_world.dart
    Test

--- 01_hello_world.dart
+++ 01_hello_world.dart
@@ -1,12 +1,21 @@
 void main() {
-  
 //   var myName = 'Fernando';
 //   late final myName;
 //   String myName = 'Fernando';
   const myName = 'Fernando';
-    
-  
+
   print('Hola $myName');
-  print('Hola ${ myName.toUpperCase() }');
-  print('Hola ${ 1 + 1 }');
+  print('Hola ${myName.toUpperCase()}');
+  print('Hola ${1 + 1}');
+}
+
+void main() {
+//   var myName = 'Fernando';
+//   late final myName;
+//   String myName = 'Fernando';
+  const myName = 'Fernando';
+
+  print('Hola $myName');
+  print('Hola ${myName.toUpperCase()}');
+  print('Hola ${1 + 1}');
 }
